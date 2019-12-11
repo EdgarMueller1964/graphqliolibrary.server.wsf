e5c5fce54d87216f448add8fb9a60653e36061b1
EdgarMueller1964 <50403858+EdgarMueller1964@users.noreply.github.com>
2019-12-11 12:25:28 +0100

    Migration JUnit4 to JUnit5 starter

--- src/test/java/com/thinkenterprise/graphqlio/GraphQlIoApplicationTests.java
+++ src/test/java/com/thinkenterprise/graphqlio/GraphQlIoApplicationTests.java
@@ -1,11 +1,17 @@
 package com.thinkenterprise.graphqlio;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+
+
+
+@Tag("annotations")
+@Tag("junit5")
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GraphQlIoApplicationTests {
 
